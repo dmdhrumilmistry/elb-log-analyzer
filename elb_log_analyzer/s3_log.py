fd02f34e9e495140c3dcfd8c96afe575e984c44f
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-03-09 23:46:16 +0530

    create class to download and extract logs from s3 bucket

--- elb_log_analyzer/s3_log.py
+++ elb_log_analyzer/s3_log.py
@@ -1,32 +1,77 @@
 import boto3
-from os import makedirs
-from os.path import isdir, join as path_join
-from datetime import datetime # , timedelta
+import gzip 
+from os import makedirs, remove
+from os.path import isdir,isfile, join as path_join, dirname
+from datetime import datetime , timedelta, timezone
 
-def s3_fetch_logs(bucket_name:str, prefix:str ,store_location:str, start_time:datetime, end_time:datetime=None):
-    '''
-    downloads logs from s3 bucket and stores into specified 
-    folder for provided time frame.
-    '''
-    if not isdir(store_location):
-        makedirs(store_location)
+class S3LogFetcher:
+    def __init__(self, bucket_name:str, prefix:str ) -> None:
+        self._bucket_name = bucket_name
+        self._prefix = prefix
 
-    if not end_time:
-        end_time = datetime.utcnow()
+        # create boto3 s3 client
+        self.s3_client = boto3.client('s3')
 
-    # create boto3 s3 client
-    s3 = boto3.client('s3')
+        
+    def download_log_and_extract(self, s3_file_key:str, local_file_path:str):
+        # download file
+        local_file_path_dir = dirname(local_file_path)
+        makedirs(local_file_path_dir, exist_ok=True)
+        self.s3_client.download_file(self._bucket_name, s3_file_key, local_file_path)
 
-    # list objects created in the time frame
-    response = s3.list_objects_v2(
-        Bucket=bucket_name,
-        Prefix=prefix,
-        StartTime=start_time,
-        EndTime=end_time
+        # extract file
+        if isfile(local_file_path):
+            extracted_file_path = local_file_path.removesuffix('.gz')
+            with gzip.open(local_file_path, 'rb') as f_in, open(extracted_file_path, 'wb') as f_out:
+                f_out.write(f_in.read())
+            
+            # delete .gz file after extraction
+            remove(local_file_path)
+            return True
+        
+        return False
+
+    def s3_fetch_logs(self, store_location:str, start_time:datetime, end_time:datetime):
+        '''
+        downloads logs from s3 bucket and stores into specified 
+        folder for provided time frame.
+        '''
+        if not isdir(store_location):
+            makedirs(store_location)
+
+        # list objects created in the time frame
+        response = self.s3_client.list_objects_v2(
+            Bucket=self._bucket_name,
+            Prefix=self._prefix,
+        )
+
+        # get files in provided time window
+        for obj in response['Contents']:
+            key = obj['Key']
+            modified_time:datetime = obj['LastModified']
+
+            if start_time <= modified_time  <= end_time:
+                local_file_path = path_join(store_location, key)
+
+                # download only *.log.gz files
+                if not local_file_path.endswith('.log.gz'):
+                    continue
+
+                # download file
+                self.download_log_and_extract(key, local_file_path)
+
+
+if __name__ == '__main__':
+    end_time = datetime.now(timezone.utc)
+    start_time = end_time - timedelta(hours=9)
+
+    log_fetcher = S3LogFetcher(
+        bucket_name='bucket-name',
+        prefix='AWSLogs/number/elasticloadbalancing/region/',
     )
 
-    # download files
-    for obj in response['Contents']:
-        key = obj['Key']
-        local_file_path = path_join(store_location, key)
-        s3.download_file(bucket_name, key, local_file_path)
+    log_fetcher.s3_fetch_logs(
+        store_location='./logs',
+        start_time=start_time,
+        end_time=end_time
+    )
