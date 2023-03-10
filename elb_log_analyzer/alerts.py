4b7f84c135f26c961b208cc358330ff968badb51
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-03-10 16:48:16 +0530

    support markdown in slack messages

--- elb_log_analyzer/alerts.py
+++ elb_log_analyzer/alerts.py
@@ -1,5 +1,5 @@
 from requests.sessions import Session
-
+from os.path import isfile, basename
 
 class SlackAlert:
     def __init__(self, webhook_url: str) -> None:
@@ -8,11 +8,14 @@ class SlackAlert:
         self._session = Session()
         self._webhook_url = webhook_url
 
+
     def send_message(self, message: str):
         assert isinstance(message, str)
 
         payload = {
+            'type':'mrkdwn',
             'text': message
         }
 
-        self._session.post(url=self._webhook_url, json=payload)
+        res = self._session.post(url=self._webhook_url, json=payload)
+        return res
