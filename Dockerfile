1d8e6fd99d6bb0724577499122186cc122546ad9
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-03-14 17:47:28 +0530

    create workflow using bash script
    add workflow in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,13 @@ RUN pip install -U pip
 
 # install tool
 RUN pip install elb-log-analyzer
+
+# create logs dir
+RUN mkdir -p /elb-log-analyzer/logs/latest
+RUN mkdir -p /elb-log-analyzer/analyzed_logs
+
+
+# copy workflow bashscript to /elb-log-analyzer
+COPY ./docker-workflow.sh /elb-log-analyzer/workflow.sh
+
+ENTRYPOINT ["/bin/bash", "/elb-log-analyzer/workflow.sh"]
---
+++ docker-workflow.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# declare an array of variable names
+var_names=("BUCKET_NAME" "BUCKET_PREFIX")
+
+# loop over the array and check each variable
+for var_name in "${var_names[@]}"; do
+  if [[ -v $var_name ]]; then
+    echo "$var_name exists"
+    # perform actions here
+  else
+    echo "$var_name does not exist"
+    exit -1
+  fi
+done
+
+# fetch logs from file and store in /elb-log-analyzer/logs/latest
+## NOTE: DATE_SUFFIX can create issues
+/usr/local/bin/python -m elb_log_analyzer.s3_log -b ${BUCKET_NAME} -p '${BUCKET_PREFIX}${DATE_SUFFIX}' -m ${LOG_ANALYSIS_INTERVAL} -o /elb-log-analyzer/logs/latest
+
+# analyze logs
+if [[ -v "REQUESTS_THRESHOLD" && -n ${REQUESTS_THRESHOLD} && -v "IP_ABUSE_DB_API_KEY" && -n ${IP_ABUSE_DB_API_KEY} ]]; then
+    echo -e "[*] REQUESTS_THRESHOLD and API key found\n"
+    /usr/local/bin/python -m elb_log_analyzer -i /elb-log-analyzer/logs/latest -o /elb-log-analyzer/analyzed_logs/log.json -t ${REQUESTS_THRESHOLD} -k ${IP_ABUSE_DB_API_KEY}
+else
+    echo -e "[!] REQUESTS_THRESHOLD and API key not found\n"
+    /usr/local/bin/python -m elb_log_analyzer -i /elb-log-analyzer/logs/latest -o /elb-log-analyzer/analyzed_logs/log.json
+fi
+
+# send alert
+if [[ -v "SLACK_WEBHOOK" && -n ${SLACK_WEBHOOK} ]]; then
+    "[*] Sending IP details to slack"
+    /usr/local/bin/python -m elb_log_analyzer.alerts -w ${SLACK_WEBHOOK} -f log.json
+else
+    echo -e "[!] SLACK_WEBHOOK var not found. Skipping Alert.\n"
+fi
