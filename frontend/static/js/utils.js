57c6ef9a2492fe6707991d905f547320235c32f8
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-05-27 20:03:15 +0530

    create IP Requests Count Table

--- frontend/static/js/utils.js
+++ frontend/static/js/utils.js
@@ -1,5 +1,8 @@
-var fileLoaded = false; // Flag to check if the file has already been loaded
-var jsonData = null; // Variable to store JSON file data
+// import { Tabulator } from 'tabulator-tables';
+const ipRequestCountTable = 'ip-requests-count-table';
+
+let fileLoaded = false; // Flag to check if the file has already been loaded
+let jsonData = null; // letiable to store JSON file data
 
 
 function addEventListener() {
@@ -7,11 +10,11 @@ function addEventListener() {
         if (!fileLoaded) {
             fileLoaded = true; // Set the flag to true to indicate the file has been loaded
 
-            var file = event.target.files[0];
-            var reader = new FileReader();
+            let file = event.target.files[0];
+            let reader = new FileReader();
 
             reader.onload = function (e) {
-                var jsonContent = e.target.result;
+                let jsonContent = e.target.result;
                 jsonData = JSON.parse(jsonContent);
                 // Call a function or perform any actions with the loaded JSON data
                 createDashboard(jsonData);
@@ -21,9 +24,40 @@ function addEventListener() {
     });
 }
 
+function createIPRequestCountTable(jsonData) {
+    // create list to store ip data
+    let ipsData = [];
+
+    for (let clientIP in jsonData) {
+        if (clientIP === 'total') { continue; }
+
+        // add data to list
+        ipsData.push({
+            ip: clientIP,
+            total_requests: jsonData[clientIP].total,
+            ports: jsonData[clientIP].ports,
+            user_agents: jsonData[clientIP].user_agents
+        })
+    }
+
+    // sort ip data list based on requests count
+    ipsData.sort(function (a, b) {
+        return b.total_requests - a.total_requests;
+    });
+
+
+    const IPtable = new Tabulator(`#${ipRequestCountTable}`, {
+        data: ipsData,
+        autoColumns: true, // Automatically generate columns
+        layout: "fitColumns", // Adjust column widths to fit the table width
+        height: "300px",
+    });
+
+}
+
 function createDashboard(jsonData) {
     // Display the JSON data on the page
-    console.log(jsonData);
-    var jsonDataContainer = document.getElementById('dashboard');
-    jsonDataContainer.textContent = JSON.stringify(jsonData);
+    createIPRequestCountTable(jsonData);
 }
+
+addEventListener();
